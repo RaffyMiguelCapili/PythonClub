8c4f89c7b20061dbb3aa6bd669a714a5934ba652
RaffyMiguelCapili <103232567+RaffyMiguelCapili@users.noreply.github.com>
2022-06-17 18:56:21 -0700

    Assignment 9

--- pythonclub/urls.py
+++ pythonclub/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+
+from club.views import loginmessage 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('club/', include('club.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
