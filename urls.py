5256c161065a1312d337bab863863665fbe530eb
RaffyMiguelCapili <103232567+RaffyMiguelCapili@users.noreply.github.com>
2022-06-17 18:35:28 -0700

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('members/', views.members, name= 'members'),  
-    path('meetingdetail/<int:id>', views.meetingDetail, name='detail'),
-    path('newmember/', views.newProduct, name='newmember'),
-]
