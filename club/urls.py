5ac510a16847ff469dc9d5b12b7c20f8b2362d21
RaffyMiguelCapili <103232567+RaffyMiguelCapili@users.noreply.github.com>
2022-06-17 18:54:09 -0700

    Assignment 9

--- club/urls.py
+++ club/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('members/', views.members, name= 'members'),  
     path('meetingdetail/<int:id>', views.meetingDetail, name='detail'),
     path('newmember/', views.newProduct, name='newmember'),
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
+
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+
+from club.forms import ProductForm
 from .models import Resource, TypeResource, Meeting, MeetingMinutes, Event
+from .forms import ProductForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -14,4 +18,21 @@ def meetingDetail(request, id):
     meeting=get_object_or_404(Meeting, pk=id)
     return render(request, 'club/meetingdetail.html', {'meetings' : meeting})
 
-    
+def newMember(request):
+    form=ProductForm
+
+    if request.method=='POST':
+        form=ProductForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ProductForm()
+    else:
+        form=ProductForm()
+    return render(request, 'club/newMember.html', {'form': form})
+
+def loginmessage(request):
+    return render(request, 'club/loginmessage.html')
+
+def logoutmessage(request):
+    return render(request, 'club/logoutmessage.html')
